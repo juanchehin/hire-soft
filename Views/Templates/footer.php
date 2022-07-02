e9873fbae6987493ddf5255745874ced57e94b48
chehin <chehinjuan@gmail.com>
2022-07-02 15:21:10 -0300

    cambio nombre

--- Views/Templates/footer.php
+++ Views/Templates/footer.php
@@ -3,7 +3,11 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+            <div class="text-muted">Copyright &copy; 
+            <script>
+                                document.write(new Date().getFullYear());
+                            </script> Todos los derechos reservados</a>
+            </div>
             <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
